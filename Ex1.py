24265411fb9a3910a3768276c56c5e862f53336e
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-02-22 23:06:37 +0200

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,14 @@
+#print("hello world!!")
+#print("Iam Iron Man")
+#print("No, Iam Tony Stark")
+#print("No, Iam Poppy")
+#Iam practicing Python
+
+print("""I am Iron Man.
+No, Iam Tony Stark.
+NO, Iam Poppy""")
+
+print("Iam Iron Man \n" + "No, Iam Tony Stark \n" + "No, Iam Poppy ")
+
+print("Iam Poppy\n" * 100)
+
---
+++ Ex2.py
@@ -0,0 +1,8 @@
+#Let's start a coffee shop!
+#Build robot barista
+
+print("Hello, welcome to NetworkChuck Coffee!")
+
+input("What is your name?")
+
+print("Hello ......, thank you so much for coming in today!")
